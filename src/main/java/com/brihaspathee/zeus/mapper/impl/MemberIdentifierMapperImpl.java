3341d2f93b93d04862faaa3adc4f696ac1c49833
vbalaji215-dev <vbalaji215@outlook.com>
2022-09-11 07:18:55 -0400

    ZEUS-118 created mapper for identifiers

---
+++ src/main/java/com/brihaspathee/zeus/mapper/impl/MemberIdentifierMapperImpl.java
@@ -0,0 +1,87 @@
+package com.brihaspathee.zeus.mapper.impl;
+
+import com.brihaspathee.zeus.domain.entity.Member;
+import com.brihaspathee.zeus.domain.entity.MemberIdentifier;
+import com.brihaspathee.zeus.mapper.interfaces.MemberIdentifierMapper;
+import com.brihaspathee.zeus.web.model.MemberIdentifierDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Created in Intellij IDEA
+ * User: Balaji Varadharajan
+ * Date: 11, September 2022
+ * Time: 7:14 AM
+ * Project: Zeus
+ * Package Name: com.brihaspathee.zeus.mapper.impl
+ * To change this template use File | Settings | File and Code Template
+ */
+@Slf4j
+@Component
+public class MemberIdentifierMapperImpl implements MemberIdentifierMapper {
+
+    /**
+     * Convert identifier dto to identifier entity
+     * @param memberIdentifierDto
+     * @return
+     */
+    @Override
+    public MemberIdentifier identifierDtoToIdentifier(MemberIdentifierDto memberIdentifierDto) {
+        if(memberIdentifierDto == null){
+            return null;
+        }
+        MemberIdentifier memberIdentifier = MemberIdentifier.builder()
+                .memberIdentifierSK(memberIdentifierDto.getMemberIdentifierSK())
+                .member(Member.builder().memberSK(memberIdentifierDto.getMemberSK()).build())
+                .identifierTypeCode(memberIdentifierDto.getIdentifierTypeCode())
+                .identifierValue(memberIdentifierDto.getIdentifierValue())
+                .createdDate(memberIdentifierDto.getCreatedDate())
+                .updatedDate(memberIdentifierDto.getUpdatedDate())
+                .build();
+        return memberIdentifier;
+    }
+
+    /**
+     * Convert identifier entity to identifier dto
+     * @param memberIdentifier
+     * @return
+     */
+    @Override
+    public MemberIdentifierDto identifierToIdentifierDto(MemberIdentifier memberIdentifier) {
+        if(memberIdentifier == null){
+            return null;
+        }
+        MemberIdentifierDto memberIdentifierDto = MemberIdentifierDto.builder()
+                .memberIdentifierSK(memberIdentifier.getMemberIdentifierSK())
+                .memberSK(memberIdentifier.getMember().getMemberSK())
+                .identifierTypeCode(memberIdentifier.getIdentifierTypeCode())
+                .identifierValue(memberIdentifier.getIdentifierValue())
+                .createdDate(memberIdentifier.getCreatedDate())
+                .updatedDate(memberIdentifier.getUpdatedDate())
+                .build();
+        return memberIdentifierDto;
+    }
+
+    /**
+     * Convert identifier dtos to identifier entities
+     * @param memberIdentifierDtos
+     * @return
+     */
+    @Override
+    public Set<MemberIdentifier> identifierDtosToIdentifiers(Set<MemberIdentifierDto> memberIdentifierDtos) {
+        return memberIdentifierDtos.stream().map(this::identifierDtoToIdentifier).collect(Collectors.toSet());
+    }
+
+    /**
+     * Convert identifier entities to identifier dtos
+     * @param memberIdentifiers
+     * @return
+     */
+    @Override
+    public Set<MemberIdentifierDto> identifierToIdentifierDtos(Set<MemberIdentifier> memberIdentifiers) {
+        return memberIdentifiers.stream().map(this::identifierToIdentifierDto).collect(Collectors.toSet());
+    }
+}
---
+++ src/main/java/com/brihaspathee/zeus/mapper/interfaces/MemberIdentifierMapper.java
@@ -0,0 +1,46 @@
+package com.brihaspathee.zeus.mapper.interfaces;
+
+import com.brihaspathee.zeus.domain.entity.MemberIdentifier;
+import com.brihaspathee.zeus.web.model.MemberIdentifierDto;
+
+import java.util.Set;
+
+/**
+ * Created in Intellij IDEA
+ * User: Balaji Varadharajan
+ * Date: 11, September 2022
+ * Time: 7:09 AM
+ * Project: Zeus
+ * Package Name: com.brihaspathee.zeus.mapper.interfaces
+ * To change this template use File | Settings | File and Code Template
+ */
+public interface MemberIdentifierMapper {
+
+    /**
+     * Convert identifier dto to identifier entity
+     * @param memberIdentifierDto
+     * @return
+     */
+    MemberIdentifier identifierDtoToIdentifier(MemberIdentifierDto memberIdentifierDto);
+
+    /**
+     * Convert identifier entity to identifier dto
+     * @param memberIdentifier
+     * @return
+     */
+    MemberIdentifierDto identifierToIdentifierDto(MemberIdentifier memberIdentifier);
+
+    /**
+     * Convert identifier dtos to identifier entities
+     * @param memberIdentifierDtos
+     * @return
+     */
+    Set<MemberIdentifier> identifierDtosToIdentifiers(Set<MemberIdentifierDto> memberIdentifierDtos);
+
+    /**
+     * Convert identifier entities to identifier dtos
+     * @param memberIdentifiers
+     * @return
+     */
+    Set<MemberIdentifierDto> identifierToIdentifierDtos(Set<MemberIdentifier> memberIdentifiers);
+}
