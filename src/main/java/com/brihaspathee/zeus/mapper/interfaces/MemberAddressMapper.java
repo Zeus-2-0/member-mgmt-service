56280fc8ef97f291ed6cd7b0ecc003660ee2cba7
vbalaji215-dev <vbalaji215@outlook.com>
2022-09-11 07:30:55 -0400

    ZEUS-119 created mapper for address

---
+++ src/main/java/com/brihaspathee/zeus/mapper/interfaces/MemberAddressMapper.java
@@ -0,0 +1,46 @@
+package com.brihaspathee.zeus.mapper.interfaces;
+
+import com.brihaspathee.zeus.domain.entity.MemberAddress;
+import com.brihaspathee.zeus.web.model.MemberAddressDto;
+
+import java.util.Set;
+
+/**
+ * Created in Intellij IDEA
+ * User: Balaji Varadharajan
+ * Date: 11, September 2022
+ * Time: 7:20 AM
+ * Project: Zeus
+ * Package Name: com.brihaspathee.zeus.mapper.interfaces
+ * To change this template use File | Settings | File and Code Template
+ */
+public interface MemberAddressMapper {
+
+    /**
+     * Convert address dto to address entity
+     * @param addressDto
+     * @return
+     */
+    MemberAddress addressDtoToAddress(MemberAddressDto addressDto);
+
+    /**
+     * Convert address entity to address dto
+     * @param address
+     * @return
+     */
+    MemberAddressDto addressToAddressDto(MemberAddress address);
+
+    /**
+     * Convert address dtos to address entities
+     * @param addressDtos
+     * @return
+     */
+    Set<MemberAddress> addressDtosToAddresses(Set<MemberAddressDto> addressDtos);
+
+    /**
+     * Convert address entities to address dtos
+     * @param addresses
+     * @return
+     */
+    Set<MemberAddressDto> addressesToAddressDtos(Set<MemberAddress> addresses);
+}
