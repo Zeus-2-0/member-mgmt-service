b9e07f10d9fead89d7f9215e4e22e3b82ac8042a
vbalaji215-dev <vbalaji215@outlook.com>
2023-04-21 21:12:08 -0400

    ZEUS-264 updated test cases

--- src/main/java/com/brihaspathee/zeus/domain/entity/MemberPremium.java
+++ src/main/java/com/brihaspathee/zeus/domain/entity/MemberPremium.java
@@ -97,16 +97,16 @@ public class MemberPremium {
                 '}';
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MemberPremium that = (MemberPremium) o;
-        return memberPremiumSK.equals(that.memberPremiumSK);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(memberPremiumSK);
-    }
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        MemberPremium that = (MemberPremium) o;
+//        return memberPremiumSK.equals(that.memberPremiumSK);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(memberPremiumSK);
+//    }
 }
